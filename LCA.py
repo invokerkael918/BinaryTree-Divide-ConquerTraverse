f00b03f1b8470c90f36ec4e81fd1bea74ebf6229
yuancao <sam.cao918@gmail.com>
2019-11-08 17:43:20 -0500

    加了注释

--- LCA.py
+++ LCA.py
@@ -1,36 +1,31 @@
-"""
-Definition of TreeNode:
-class TreeNode:
-    def __init__(self, val):
-        self.val = val
-        self.left, self.right = None, None
-"""
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution:
-    """
-    @param: root: The root of the binary search tree.
-    @param: A: A TreeNode in a Binary.
-    @param: B: A TreeNode in a Binary.
-    @return: Return the least common ancestor(LCA) of the two nodes.
-    """
-
-    def lowestCommonAncestor(self, root, A, B):
-        # write your code here
-
-        if root is None or root == A or root == B:
+    def lowestCommonAncestor(self, root: 'TreeNode', A: 'TreeNode', B: 'TreeNode') -> 'TreeNode':
+        if root is None:
+            return None
+        if root == A or root == B:
+            # 找到了A or B return root（A，or B)
             return root
-
         leftPath = self.lowestCommonAncestor(root.left, A, B)
+        # 向左递归
         rightPath = self.lowestCommonAncestor(root.right, A, B)
-
+        # 向右递归
         if leftPath is not None and rightPath is not None:
+            # 左右都不为None，此时root为LCA
             return root
 
         if leftPath is not None:
+            # 左边是None从此层return 右边
             return leftPath
 
         if rightPath is not None:
+            # 右边是None从此层return 左边
             return rightPath
-
+        # 都是None return None
         return None
